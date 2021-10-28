f97bf07692a5f6e0c3ff23820c4f261df412d08b
braxtonhall <braxtonjhall@gmail.com>
2021-10-27 17:12:13 -0700

    Init test suite

---
+++ crdtree/test/CRDTree.spec.ts
@@ -0,0 +1,26 @@
+import {expect} from "chai";
+import {Done} from "mocha";
+
+describe("CRDTree", () => {
+
+	describe("CRDT", () => {
+
+		describe("assign/insert/render", () => {
+
+		});
+
+		describe("merge", () => {
+
+		});
+
+		describe("clone", () => {
+
+		});
+
+		describe("onUpdate", () => {
+			it("should eventually call onUpdate", (done) => {
+				done(); // TODO
+			});
+		});
+	});
+});
