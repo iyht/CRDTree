d558d2630bfa7f8105a236c27c9d7959eb0f6894
Ray Zhang <raybot@students.cs.ubc.ca>
2021-11-29 12:28:06 -0800

    Added visited accumulator to prevent infinite loops in branch predecessor checking

--- crdtree/src/Branch.ts
+++ crdtree/src/Branch.ts
@@ -7,17 +7,20 @@ const checkCausallyRelevant = (change: Change, branch: BranchID, map: BranchMap)
 	if (change.branch == branch) return true;
 	if (map.get(branch) == undefined) return false;
 	return Array.from(map.get(branch)).map(([predecessorID, causalBoundary]) => {
-		return checkCausallyRelevantParents(change, predecessorID, causalBoundary, map)
+		return checkCausallyRelevantParents(change, predecessorID, causalBoundary, map, new Set(map.keys()))
 	}).reduce((b1, b2) => {return b1 || b2}, false)
 }
 
-const checkCausallyRelevantParents = (change: Change, branch: BranchID, causalBoundary: ID, map: BranchMap): boolean => {
+const checkCausallyRelevantParents = (change: Change, branch: BranchID, causalBoundary: ID, map: BranchMap, visited: Set<BranchID>): boolean => {
 	// TODO replace toID(change) with changeID instead of recomputing
 	if (toID(change) == causalBoundary) return true;
 	if (change.branch == branch) return nameLt(toID(change), causalBoundary);
 	if (map.get(branch) == undefined) return false;
-	return Array.from(map.get(branch)).map(([predecessorID, causalBoundary]) => {
-		return checkCausallyRelevantParents(change, predecessorID, causalBoundary, map)
+	let nextPreds = Array.from(map.get(branch)).filter(
+		([predId, boundary]) => {!visited.has(predId)});
+	nextPreds.forEach(([predId, boundary]) => {visited.add(predId)});
+	return nextPreds.map(([predecessorID, causalBoundary]) => {
+		return checkCausallyRelevantParents(change, predecessorID, causalBoundary, map, visited)
 	}).reduce((b1, b2) => {return b1 || b2}, false)
 }
 
