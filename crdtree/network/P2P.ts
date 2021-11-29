0d88cfd515a487f76fed3766f68c85414d106285
Haotian Yang <haotian.yang.cs@gmail.com>
2021-11-28 21:13:30 -0800

    clean up

--- crdtree/network/P2P.ts
+++ crdtree/network/P2P.ts
@@ -56,32 +56,4 @@ const createNode = async (bootstrapHost = null) => {
 
   return node
 }
-
-
-// ;(async () => {
-
-//   const [node1, node2] = await Promise.all([
-//         createNode(), 
-//         createNode()
-//   ])
-//   console.log(node1.peerId.toB58String())
-//   console.log(node2.peerId.toB58String())
-// })();
-
 export {createNode};
-
-// console.log(node.peerId.toB58String())
-// ;(async () => {
-//   const [node1, node2] = await Promise.all([
-//     createNode(),
-//     createNode()
-//   ])
-
-//   node1.on('peer:discovery', (peerId) => console.log('Discovered:', peerId.toB58String()))
-//   node2.on('peer:discovery', (peerId) => console.log('Discovered:', peerId.toB58String()))
-
-//   await Promise.all([
-//     node1.start(),
-//     node2.start()
-//   ])
-// })();
