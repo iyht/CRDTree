af5b28c3f4dc17b609264655344581cbc72130f3
braxtonhall <braxtonjhall@gmail.com>
2021-12-06 10:34:28 -0800

    Init demo

---
+++ crdtree/src/index.ts
@@ -0,0 +1,4 @@
+import {ICRDTree} from "./API";
+import {CRDTree} from "./CRDTree";
+
+export {ICRDTree, CRDTree};
---
+++ demo/public/client.js
@@ -0,0 +1,21 @@
+const name = prompt('Whats your name?');
+
+const textarea = document.querySelector('textarea');
+const input = document.querySelector('input');
+input.placeholder = `Say something, ${name}`;
+
+const webSocket = new WebSocket(`wss://${location.host}`);
+
+webSocket.addEventListener('open', function(e) {
+	// When we receive a message from the server,
+	// we show it
+	webSocket.addEventListener('message', function(e) {
+		textarea.value += e.data + "\n";
+	});
+
+	// We send all messages to the server
+	input.addEventListener('change', function(e) {
+		webSocket.send(`${name}: ${input.value}`);
+		input.value = "";
+	});
+});
