ca40507111378449114d956cbed68350700d54eb
braxtonhall <braxtonjhall@gmail.com>
2021-12-18 14:25:10 -0800

    Remove unused scripts

--- network/test/interactive/Bootstrap.ts
+++
@@ -1,6 +0,0 @@
-// import {RootNetwork} from "../../src/RootNetwork";
-// import {CRDTree} from "crdtree";
-//
-// let rn: RootNetwork;
-// rn = new RootNetwork(new CRDTree());
-// rn.createBootstrapNode();
